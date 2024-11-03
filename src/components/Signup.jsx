09613d7da736f382a40e360051236224c883a0f8
Suvesh Pandey <jpsuvesh29@gmail.com>
2024-11-03 22:57:12 +0530

    refined signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -71,17 +71,17 @@ export const Signup = () => {
             className='text-6xl font-serif  bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent mb-5'>Gemini</h1>
             <form
                 onSubmit={handleSubmit}
-                className='w-[90%] sm:w-[40%] md:w-[30%]  h-auto bg-slate-200 bg-opacity-50 rounded-md px-3 py-5 flex flex-col items-center space-y-3 border-[1px] border-slate-300 shadow-slate-300 shadow-sm'>
+                className='w-[90%] sm:w-[50%] md:w-[50%]  h-auto bg-slate-200 bg-opacity-50 rounded-md px-3 py-5 flex flex-col items-center space-y-3 border-[1px] border-slate-300 shadow-slate-300 shadow-sm'>
                 <input
                     type='text'
-                    className='w-[100%] py-[6px] px-3 rounded-md outline-none border-[1px] border-opacity-50 hover:border-opacity-100 border-slate-400 text-sm transition-all duration-300'
+                    className='w-[100%] sm:py-[6px] py-[10px] px-3 rounded-md outline-none border-[1px] border-opacity-50 hover:border-opacity-100 border-slate-400 text-sm transition-all duration-300'
                     placeholder='Email'
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <div className='w-[100%] justify-start items-center  '>
                     <input
                         type='password'
-                        className='w-[100%] py-[6px] px-3 rounded-md outline-none border-[1px] border-opacity-50 hover:border-opacity-100 border-slate-400 text-sm transition-all duration-300'
+                        className='w-[100%] sm:py-[6px] py-[10px] px-3 rounded-md outline-none border-[1px] border-opacity-50 hover:border-opacity-100 border-slate-400 text-sm transition-all duration-300'
                         placeholder={( isSignUp ) ? `Set Your Password` : `Password`}
                         onChange={(e) => setPassword(e.target.value)}
                     />
@@ -89,14 +89,14 @@ export const Signup = () => {
                 {isSignUp && ( // Only show the username input for sign up
                     <input
                         type='text'
-                        className='w-[100%] py-[6px] px-3  rounded-md outline-none border-[1px] border-opacity-50 hover:border-opacity-100 border-slate-400 text-sm transition-all duration-300'
+                        className='w-[100%] sm:py-[6px] py-[10px] px-3  rounded-md outline-none border-[1px] border-opacity-50 hover:border-opacity-100 border-slate-400 text-sm transition-all duration-300'
                         placeholder={isSignUp ? 'Set Your Username' : 'Username'}
                         onChange={(e) => setUsername(e.target.value)}
                     />
                 )}
                 <button
                     type='submit'
-                    className=' text-[#fbfbfe] sm:w-[100%] w-[100%] py-[6px] rounded-full font-semibold hover:bg-[#2f27ce] bg-[#443dff] text-sm transition-all duration-200'>
+                    className=' text-[#fbfbfe] sm:w-[100%] w-[100%] sm:py-[6px] py-[10px] rounded-full font-semibold hover:bg-[#2f27ce] bg-[#443dff] text-sm transition-all duration-200'>
                     {isSignUp ? "Sign Up" : "Sign In"}
                 </button>
                 <p className='text-sm text-slate-400 mt-2'>
